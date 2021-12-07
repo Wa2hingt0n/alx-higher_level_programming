d71d5b977c15e642cdfc86ddfbde25896fe4806d
Washington Obungu <tobungu@gmail.com>
2021-12-07 08:23:11 -0800

    Created a module that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """Initializes the class Square.
+
+        Args:
+            size (int): The size of the square
+
+        """
+
+        self.__size = size
