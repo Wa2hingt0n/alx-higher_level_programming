c1b0ab5bfe779a9de67b2b4e2e1dac4302ef5095
Washington Obungu <tobungu@gmail.com>
2021-12-07 11:27:34 -0800

    Created a class size and added a getter and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size=0):
+        """Initializes the square by size
+
+        Args:
+            size: Size of the square
+
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Property to retrieve the private instance of size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Property setting method for the size of the square
+
+        Args:
+            value: The set value of the square.
+
+        Raises:
+            TypeError: If the size is not an integer
+            ValueError: If the size is less than 0
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Computes the area of the square
+
+        Returns:
+            int: The area of the square
+
+        """
+        return (self.__size ** 2)
