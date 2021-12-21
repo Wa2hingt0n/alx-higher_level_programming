67d3f307d8fbd196fa5795c9fdc06d67d3f87deb
Washington Obungu <tobungu@gmail.com>
2021-12-21 06:03:12 -0800

    Created a function that reads a file and prints it to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a function to read files and print result to stdout"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout"""
+    with open(filename, mode="r", encoding="utf-8") as a_file:
+        print(a_file.read(), end="")
