8e04bab270dcfed267b752da664370cf248d693a
Washington Obungu <tobungu@gmail.com>
2021-12-21 11:48:35 -0800

    Resolved pycodestyle compliance

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,17 +7,17 @@ def pascal_triangle(n):
 
     Returns: List of integers representing the Pascal's triangle
     """
-    triangle = []
+    triang = []
     if n <= 0:
-        return []
+        return triang
     else:
         for i in range(n):
             if i == 0:
-                triangle.append([1])
+                triang.append([1])
             elif i == 1:
-                triangle.append([1, 1])
+                triang.append([1, 1])
             else:
-                triangle.append([1 for j in range(i + 1)])
-                for k in range(1, len(triangle[i]) - 1):
-                    triangle[i][k] = triangle[i - 1][k - 1] + triangle[i - 1][k]
-    return triangle
+                triang.append([1 for j in range(i + 1)])
+                for k in range(1, len(triang[i]) - 1):
+                    triang[i][k] = triang[i - 1][k - 1] + triang[i - 1][k]
+    return triang
