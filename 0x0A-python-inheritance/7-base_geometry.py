6efb49bbabea2fe937fbd395bd42b6babfe0fcb7
Washington Obungu <tobungu@gmail.com>
2021-12-29 05:14:35 -0800

    Modified object instance check

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -16,7 +16,7 @@ class BaseGeometry:
             name (str): The name that is always a string
             value (int): The value to be validated
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
