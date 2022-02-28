ed276fd398384bf63aea3e7451d3c008e4c5a9cd
Washington Obungu <tobungu@gmail.com>
2022-02-28 07:40:51 -0800

    Added a function that concatenates the two arguments passed to it

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let myString = process.argv[2] + ' is ' + process.argv[3];
+
+if (!(process.argv[2])) {
+  process.argv[2] = 'undefined';
+  process.argv[3] = 'undefined';
+} else if (!(process.argv[3])) {
+  process.argv[3] = 'undefined';
+}
+
+console.log(myString);
