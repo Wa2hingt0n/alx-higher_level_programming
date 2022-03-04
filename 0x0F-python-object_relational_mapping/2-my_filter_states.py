7195007cbbaf9e93d45b41fcd1dc92d4f60aab0b
Washington Obungu <tobungu@gmail.com>
2022-03-04 03:53:41 -0800

    Added a script that takes a name to be searched as the fourh argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Takes an argument and displays all values in the `states` table of the
+    hbtn_0e_0_usa database where `name` matches the argument.
+
+    Usage: <2-my_filter_states> <username> <password> <database> <name>
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE states.name='{}' ORDER BY states.id"
+        .format(sys.argv[4]))
+
+    query_row = cur.fetchall()
+    for row in query_row:
+        print(row)
+
+    cur.close()
+    conn.close()
