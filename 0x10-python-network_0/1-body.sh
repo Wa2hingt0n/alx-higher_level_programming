75b4c8822a0e58be020bcbee6df22f1f633ff698
Washington Obungu <tobungu@gmal.com>
2022-03-17 08:41:47 -0700

    Limited script to 3 lines

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Displays the body of an HTTP GET request response if status code is 200
-STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" $1)
-
-if [ "$STATUS_CODE" == "200" ]; then
-    curl -s $1
-fi
+if [ $(curl -s -o /dev/null -w "%{http_code}" $1) == "200" ]; then curl -s $1; fi
