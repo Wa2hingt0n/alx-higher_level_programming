71011ba2a34bc24b6eb338936c2fa17a591d26a9
Washington Obungu <tobungu@gmail.com>
2022-03-18 03:36:05 -0700

    Added provision to prevent script from being executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,14 @@
 """ Takes in a URL, sends a request to the URL and displays the value of the
     'X-Request-Id' variable found in the header of the response.
 """
-import urllib.request
-import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    content = response.info()
-    value = content.get('X-Request-Id')
-    print(value)
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+
+
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        content = response.info()
+        value = content.get('X-Request-Id')
+        print(value)
