13d5d97a2a93e41f7f8461d7004dcf6ba3f30892
Washington Obungu <tobungu@gmail.com>
2022-03-18 03:08:26 -0700

    Added a script that fetches a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Fetches the url 'https://alx-intranet.hbtn.io/status' """
+import urllib.request
+
+
+with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    content = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode()))
