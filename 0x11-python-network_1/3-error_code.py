edb8080fba0b0923ad068203639ed222e5b13046
Washington Obungu <tobungu@gmail.com>
2022-03-18 05:57:05 -0700

    Added scripts that handles errors

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Takes a URL, sends a request and displays the body of the response """
+
+
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    from urllib.error import HTTPError
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            content = response.read()
+            print(content)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
